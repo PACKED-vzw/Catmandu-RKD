ac4abceb6a1ac84249e049368683a950d217dcbd
Pieter De Praetere <pieter@packed.be>
2017-01-05 14:19:11 +0100

    Refactored: moved API code to Search.pm

--- lib/Catmandu/Store/RKD/API/Name.pm
+++ lib/Catmandu/Store/RKD/API/Name.pm
@@ -2,84 +2,21 @@ package Catmandu::Store::RKD::API::Name;
 
 use Moo;
 use LWP::UserAgent;
-use XML::Struct qw(readXML);
 
 use Catmandu::Sane;
 
-use Catmandu::Store::RKD::API::Parse;
-use Catmandu::Store::RKD::API::Extract;
+use Catmandu::Store::RKD::API::Search;
 
 has name_to_search => (is => 'ro', required => 1);
 
-has engine   => (is => 'lazy');
 has results  => (is => 'lazy');
-
-sub _build_engine {
-    my $self = shift;
-    my $engine = LWP::UserAgent->new();
-    return $engine;
-}
 
 sub _build_results {
     my $self = shift;
-    my $items_raw = $self->__search();
-    my $items = [];
-    
-    # The results are in $items_raw->{'items'}
-    my $parser = Catmandu::Store::RKD::API::Parse->new(results => $items_raw);
-    my $parsed_items = $parser->items;
-    push @{$items}, @{$parsed_items->{'items'}};
-
-    # The API paginates, but we want all the results. So continue as long as there are items
-    while ($parsed_items->{'total'} > $parsed_items->{'start'} * $parsed_items->{'per_page'}) {
-
-        my $startIndex = $parsed_items->{'start'} + $parsed_items->{'per_page'};
-        $items_raw = $self->__search($startIndex);
-
-        $parser = Catmandu::Store::RKD::API::Parse->new(results => $items_raw);
-        $parsed_items = $parser->items;
-
-        push @{$items}, @{$parsed_items->{'items'}};
-    }
-
-    # Return [{
-    #    guid        => foo,
-    #    title       => bar,
-    #    description => biz,
-    #    artist_link => xyz
-    #}]
-    my $extracter = Catmandu::Store::RKD::API::Extract->new(results => $items);
-    return $extracter->items;
-}
-
-##
-# Query the API
-sub __search {
-    my ($self, $startIndex) = @_;
-    my $url;
-    if (defined($startIndex)) {
-        my $template = 'http://opendata.rkd.nl/opensearch/artists/eac-cpf?q=naamdeel:(%s)&startIndex=%s';
-        $url = sprintf($template, $self->name_to_search, $startIndex);
-    } else {
-        my $template = 'http://opendata.rkd.nl/opensearch/artists/eac-cpf?q=naamdeel:(%s)';
-        $url = sprintf($template, $self->name_to_search);
-    }
-    my $response = $self->engine->get($url);
-    if ($response->is_success) {
-        return $response->decoded_content;
-    } else {
-        Catmandu::HTTPError->throw({
-                code             => $response->code,
-                message          => $response->status_line,
-                url              => $response->request->uri,
-                method           => $response->request->method,
-                request_headers  => [],
-                request_body     => $response->request->decoded_content,
-                response_headers => [],
-                response_body    => $response->decoded_content,
-            });
-        return undef;
-    }
+    my $template = 'http://opendata.rkd.nl/opensearch/artists/eac-cpf?q=naamdeel:(%s)';
+    my $url = sprintf($template, $self->name_to_search);
+    my $search = Catmandu::Store::RKD::API::Search->new(url => $url);
+    return $search->results;
 }
 
 1;
